0e523dd8501fe09502e94f54bf8368e252c3ee69
Ragavendra Dani <ragavendra.dani@gmail.com>
2019-06-11 05:34:22 -0700

    learned class in js

---
+++ classtest.js
@@ -0,0 +1,35 @@
+var object1 = {value:10};
+var object2 = object1;
+var object3 = {value:10};
+
+//context vs scope
+const object4 = {
+    a: function(){  
+        console.log(this);
+    }
+}
+
+//instantiation
+class Player {
+    constructor(name,type){
+        this.name = name;
+        this.type = type;
+    }
+    introduce(){
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+    }
+}
+class Wizard extends Player{
+    constructor(name,type){
+        super(name,type);
+    }
+    play(){
+        console.log(`WEEEEEEEEEE I'm a ${this.type}`);
+    
+    }
+}
+const wizard1 = new Wizard('Shelly','Healer');
+const wizard2 = new Wizard('Shawn','Dark Magic');
+
+wizard1.introduce();
+wizard1.play();
