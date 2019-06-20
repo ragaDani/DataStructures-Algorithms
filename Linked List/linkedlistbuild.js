e21d90082b72f7f48c1d6bd36a27389789f62443
Ragavendra Dani <ragavendra.dani@gmail.com>
2019-06-20 15:12:17 -0700

    Node changes

--- Linked List/linkedlistbuild.js
+++ Linked List/linkedlistbuild.js
@@ -1,3 +1,9 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 class LinkedList {
     constructor(value) {
         this.head = {
@@ -9,18 +15,24 @@ class LinkedList {
     }
     append(value) {
         // Code here
-        this.head.next = this.head;
-        this.head = {
-            value: value,
-            next: null
-        }
-        this.tail = this.head
+        const newNode = new Node(value);
+        this.tail.next = newNode; // remember that head.next is also changing to newNode
+        this.tail = newNode;
         this.length+=1;
+        return this;
     }
-
+    prepend(value){
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode
+        this.length+=1;
+        return this;
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.append(19);
+myLinkedList.prepend(55);
 console.log(myLinkedList);
