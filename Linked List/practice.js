7dfcb8e782b0e0f40dfc92c2a9ec576ad8d6a22e
Raga Dani <raga.aidev@gmail.com>
2019-07-03 18:36:46 -0700

    LL practice completed

--- Linked List/practice.js
+++ Linked List/practice.js
@@ -31,13 +31,65 @@ class LL {
         this.length++;
     }
     printList(){
-        
+        let array = []
+        var currentNode = this.head;
+        while(currentNode !=null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(array);
+    }
+    insert(index,value){
+        const newNode = new Node(value);
+        var currentNode = this.head;
+        let loopIterator = 1;
+        while(loopIterator != index){
+            if(loopIterator === index -1){
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+            }
+            loopIterator++;
+            currentNode = currentNode.next;
+        }
+        this.length++;
+        return this;
+    }
+    remove(index){
+        var currentNode = this.head;
+        let loopIterator = 1;
+        while(loopIterator!=index){
+            if(loopIterator === index -1){
+                currentNode.next = currentNode.next.next;
+            }
+            loopIterator++;
+            currentNode = currentNode.next;
+        }
+        this.length--;
+        return this;
+    }
+    reverse(){
+        this.tail = this.head;
+        let first = this.head;
+        let second = first.next;
+        while(second){
+            const temp = second.next;
+            second.next = first;
+            first = second;
+            second = temp;
+        }
+        this.head.next = null;
+        this.head = first;
     }
 }
+
 const myLL = new LL();
 myLL.append(10);
-//myLL.append(20);
-//myLL.append(30);
+myLL.append(20);
+myLL.append(30);
 myLL.prepend(50);
 myLL.prepend(60);
-console.log(myLL);
+myLL.insert(3,'Oi');
+myLL.remove(3);
+myLL.reverse();
+//console.log(myLL);
+myLL.printList();
