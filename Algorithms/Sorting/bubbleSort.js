8c4865ecbc86aed52cbf8b178b5f5c6001ab1b1f
Raga Dani <raga.aidev@gmail.com>
2019-07-11 20:23:52 -0700

    Tried Bubble sort

--- Algorithms/Sorting/bubbleSort.js
+++ Algorithms/Sorting/bubbleSort.js
@@ -1,3 +1,15 @@
 function bubbleSort(array){
-    for(let )
+    for(let i = array.length-1;i>0;i--){
+        for (let j=0;j<i;j++){
+            if(array[j]>array[j+1]){
+                swap(aray[j],array[j+1]);
+            }
+        }
+    }
+}
+function swap(value1,value2){
+    let temp = value1;
+    value1 = value2;
+    value2 = temp;
+    return this;
 }
