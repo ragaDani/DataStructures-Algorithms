07aece77776b68fa0a843349512fe6c9b8bbd7e2
Ragavendra Dani <ragavendra.dani@gmail.com>
2019-09-20 04:49:49 -0700

    BFS

--- Data Structures/Trees/test.js
+++ Data Structures/Trees/test.js
@@ -7,4 +7,5 @@ myTree.insert(11);
 myTree.insert(9);
 myTree.remove(100);
 console.log(myTree);
+console.log(myTree.breadthFirstSearch())
 //console.log(myTree.lookup(9));
--- Data Structures/Trees/treebuild.js
+++ Data Structures/Trees/treebuild.js
@@ -122,7 +122,39 @@ class BinarySearchTree{
             return true;
         }
     }
-}
+    }
+    breadthFirstSearch(){
+        let currentNode = this.root;
+        let list = []
+        let queue = []
+        queue.push(currentNode);
+
+        while(queue.length>0) {
+            currentNode = queue.shift();
+            list.push(currentNode.value);
+            if(currentNode.left){
+                queue.push(currentNode.left);
+            }
+            if(currentNode.right){
+                queue.push(currentNode.right);
+            }
+        }
+        return list;
+    }
+    breadthFirstSearchR(queue, list){
+        if(!queue,length){
+            return list;
+        }
+        let currentNode = this.queue.shift();
+        list.push(currentNode.value)
+        if(currentNode.left){
+            queue.push(currentNode.left);
+        }
+        if(currentNode.right){
+            queue.push(currentNode.right);
+        }
+        return this.breadthFirstSearchR(queue,list);
+    }
 }
 
 module.exports = BinarySearchTree;
